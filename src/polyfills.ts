93e0671cc550cd358096fa9cc7fa0e6590e33e39
Surajcachelabs <suraj@cachelabs.io>
2025-11-27 14:29:52 +0530

    Implement local File polyfill

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,6 +1,29 @@
-const undici = require('undici') as { File?: typeof globalThis.File };
+// Minimal WHATWG File implementation for Node 18 runtimes.
+type FileBits = BlobPart[];
+type FileInit = {
+  lastModified?: number;
+  type?: string;
+};
 
-// Provide File global for Node 18 runtimes where it is missing.
-if (typeof globalThis.File === 'undefined' && undici.File) {
-  (globalThis as typeof globalThis & { File?: typeof undici.File }).File = undici.File;
+if (typeof globalThis.File === 'undefined' && typeof Blob !== 'undefined') {
+  class FilePolyfill extends Blob {
+    readonly name: string;
+    readonly lastModified: number;
+    readonly webkitRelativePath = '';
+
+    constructor(fileBits: FileBits, fileName: string, options: FileInit = {}) {
+      if (!fileName) {
+        throw new TypeError('File name must be provided');
+      }
+      super(fileBits, { type: options.type });
+      this.name = String(fileName);
+      this.lastModified = options.lastModified ?? Date.now();
+    }
+
+    get [Symbol.toStringTag]() {
+      return 'File';
+    }
+  }
+
+  (globalThis as typeof globalThis & { File?: typeof FilePolyfill }).File = FilePolyfill;
 }
